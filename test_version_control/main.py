26f85bbcf0440e1a536ada61e69633e07b25bdb2
ivan5632 <ivboudnikov@yahoo.com>
2020-02-05 18:24:23 +0930

    initial commit

---
+++ test_version_control/main.py
@@ -0,0 +1,4 @@
+from test_version_control import plc_components
+
+io = plc_components.IO(7)
+print(io.some_data)
---
+++ test_version_control/plc_components.py
@@ -0,0 +1,5 @@
+class IO:
+    """IO list"""
+
+    def __init__(self, some_data):
+        self.some_data = some_data
