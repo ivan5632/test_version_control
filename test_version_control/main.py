8cb0165ed051b83790c7522d2e818540aec97eab
ivan5632 <ivboudnikov@yahoo.com>
2021-01-03 10:34:24 +0930

    started new main.py file

--- test_version_control/main.py
+++
@@ -1,4 +0,0 @@
-from test_version_control import plc_components
-
-io = plc_components.IO(15)
-print(io.some_data)
--- test_version_control/plc_components.py
+++
@@ -1,5 +0,0 @@
-class IO:
-    """IO list"""
-
-    def __init__(self, some_data):
-        self.some_data = some_data
